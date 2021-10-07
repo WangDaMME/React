b020ebb5f794c3835124e7587d3287358bbb76a6
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2021-10-07 15:42:15 -0400

    Add files via upload

---
+++ react_basics/reactRouter路由/withRouter 包装/src/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from "react"
+const DetailData = [
+    {id:'01', content: "你好，中国"},
+    {id:'02', content: "你好，黑龙江"},
+    {id:'03', content: "你好，哈尔滨"}
+
+]
+
+export default class Detail extends Component{
+    render(){
+
+
+        // 接收 state  参数
+        const {id,title} = this.props.match.params; // state params参数 存放的位置 location.state 里
+
+
+        const findRes = DetailData.find( (detailObj)=>{
+            return detailObj.id === id;
+        }) || {}
+
+
+        return (
+            <ul>
+                <li> ID：{id}</li>
+                <li> Title：{title}</li>
+                <li> Content: {findRes.content}</li>
+
+            </ul>
+        )
+    }
+}
