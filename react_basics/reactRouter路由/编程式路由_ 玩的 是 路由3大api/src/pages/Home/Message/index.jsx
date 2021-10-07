788a5f8d6bcf543018de251122789a55db4a7d5c
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2021-10-07 15:06:16 -0400

    Add files via upload

---
+++ react_basics/reactRouter路由/编程式路由_ 玩的 是 路由3大api/src/pages/Home/Message/index.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import {Link, Route} from "react-router-dom"
+import Detail from './Detail'
+export default class Message extends Component {
+
+	// 动态生成数组 
+	state = {
+		messageArr : [
+			{id:"01", title:"消息1"},
+			{id:"02", title:"消息2"},
+			{id:"03", title:"消息3"},
+
+		]
+	}
+
+	showReplace =(id, title)=>{
+		//编写一段代码， 让其实现跳转到Detail 组件 (肯定是 不带 link, navlink 需要点击 才能跳转)
+		// 为replace跳转
+		this.props.history.replace(`/home/message/detail/${id}/${title}`)
+
+		// 携带 search 参数
+		// this.props.history.replace(`/home/message/detail?id=${id}&title=${title}`)
+
+		// // 携带 state 参数
+		// this.props.history.replace(`/home/message/detail`, {id,title}) // path, state 参数
+	}
+
+	showPush = (id, title)=>{
+		this.props.history.push(`/home/message/detail/${id}/${title}`)
+
+	}
+
+	back =()=>{
+		this.props.history.goBack()
+	}
+	forward =()=>{
+		this.props.history.goForward()
+	}
+
+	render() {
+		return (
+			<div>s
+				<ul>
+					{
+						this.state.messageArr.map( (messageItem)=>{
+							return (
+								<li key = {messageItem.id}>
+
+									{/* 向路由组件 传递 params 参数, 写成对象形式*/}
+									<Link to={`/home/message/detail/${messageItem.id}/${messageItem.title}`}>{messageItem.title}</Link>
+									&nbsp;<button onClick={()=>{this.showPush(messageItem.id, messageItem.title)}}>push查看</button>
+s									&nbsp;<button onClick={()=>this.showReplace(messageItem.id, messageItem.title)}>replace查看</button>
+
+								</li>
+							)
+
+						})
+					}
+				</ul>
+				<hr/>
+				
+
+
+				{/* 向路由组件 接收 params 参数*/}
+				<Route path="/home/message/detail/:id/:title" component={Detail} />
+				
+				<button onClick = {this.back}>Back</button>
+				<button onClick = {this.forward}>Forward</button>
+			</div>
+		)
+	}
+}
